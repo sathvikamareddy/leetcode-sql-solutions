3a7acd66087acc1fbdfbc9c4565ae36dde73d187
MAREDDY SATHVIKA <mareddysathvika02@gmail.com>
2026-05-18 15:28:24 +0530

    Solved LeetCode 1757 - Recyclable and Low Fat Products
    Added a link to the LeetCode problem for reference.

--- Easy/1757_Recyclable_and_Low_Fat_Products.sql
+++ Easy/1757_Recyclable_and_Low_Fat_Products.sql
@@ -1,7 +1,9 @@
 -- LeetCode 1757
 -- Recyclable and Low Fat Products
 -- Difficulty: Easy
+-- Link: https://leetcode.com/problems/recyclable-and-low-fat-products/
 
+-- Find products that are both low fat and recyclable
 
 SELECT product_id
 FROM Products
