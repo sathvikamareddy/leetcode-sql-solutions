d7b9615cb860b873fc88adf17dc8cf57bb8ef9bd
MAREDDY SATHVIKA <mareddysathvika02@gmail.com>
2026-05-24 08:33:14 +0530

    Solved Leetcode-15811581 Customer Who Visited but Did Not Make Any Transactions

---
+++ Easy/1581_Customer_Who_Visited_but_Did_Not_Make_Any_Transactions.sql
@@ -0,0 +1,72 @@
+-- LeetCode 1581
+-- Customer Who Visited but Did Not Make Any Transactions
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/customer-who-visited-but-did-not-make-any-transactions/
+
+-- Question:
+-- Find the IDs of the users who visited without making any transactions
+-- and the number of times they made these types of visits.
+
+-- Schema:
+
+-- Visits Table:
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+
+-- Transactions Table:
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+
+-- Example Input:
+
+-- Visits Table:
+-- +----------+-------------+
+-- | visit_id | customer_id |
+-- +----------+-------------+
+-- | 1        | 23          |
+-- | 2        | 9           |
+-- | 4        | 30          |
+-- | 5        | 54          |
+-- | 6        | 96          |
+-- | 7        | 54          |
+-- | 8        | 54          |
+-- +----------+-------------+
+
+-- Transactions Table:
+-- +----------------+----------+--------+
+-- | transaction_id | visit_id | amount |
+-- +----------------+----------+--------+
+-- | 2              | 5        | 310    |
+-- | 3              | 5        | 300    |
+-- | 9              | 5        | 200    |
+-- | 12             | 1        | 910    |
+-- | 13             | 2        | 970    |
+-- +----------------+----------+--------+
+
+-- Expected Output:
+-- +-------------+----------------+
+-- | customer_id | count_no_trans |
+-- +-------------+----------------+
+-- | 54          | 2              |
+-- | 30          | 1              |
+-- | 96          | 1              |
+-- +-------------+----------------+
+
+-- Solution:
+SELECT
+    v.customer_id,
+    COUNT(v.visit_id) AS count_no_trans
+FROM Visits v
+LEFT JOIN Transactions t
+ON v.visit_id = t.visit_id
+WHERE t.transaction_id IS NULL
+GROUP BY v.customer_id;
