4379a3939e022bb19f0c420870f28bdd6f131f9a
MAREDDY SATHVIKA <mareddysathvika02@gmail.com>
2026-05-27 09:32:08 +0530

    Solved LeetCode 620 - Not Boring Movies

---
+++ Easy/620_Not_Boring_Movies.sql
@@ -0,0 +1,46 @@
+-- LeetCode 620
+-- Not Boring Movies
+-- Difficulty: Easy
+-- Link: https://leetcode.com/problems/not-boring-movies/
+
+-- Question:
+-- Report the movies with an odd-numbered ID
+-- and a description that is not "boring".
+-- Return the result table ordered by rating in descending order.
+
+-- Schema:
+-- +----------------+----------+
+-- | Column Name    | Type     |
+-- +----------------+----------+
+-- | id             | int      |
+-- | movie          | varchar  |
+-- | description    | varchar  |
+-- | rating         | float    |
+-- +----------------+----------+
+
+-- Example Input:
+
+-- Cinema Table:
+-- +----+------------+-------------+--------+
+-- | id | movie      | description | rating |
+-- +----+------------+-------------+--------+
+-- | 1  | War        | great 3D    | 8.9    |
+-- | 2  | Science    | fiction     | 8.5    |
+-- | 3  | irish      | boring      | 6.2    |
+-- | 4  | Ice song   | Fantacy     | 8.6    |
+-- | 5  | House card | Interesting | 9.1    |
+-- +----+------------+-------------+--------+
+
+-- Expected Output:
+-- +----+------------+-------------+--------+
+-- | id | movie      | description | rating |
+-- +----+------------+-------------+--------+
+-- | 5  | House card | Interesting | 9.1    |
+-- | 1  | War        | great 3D    | 8.9    |
+-- +----+------------+-------------+--------+
+
+SELECT *
+FROM Cinema
+WHERE id % 2 = 1
+AND description != 'boring'
+ORDER BY rating DESC;
